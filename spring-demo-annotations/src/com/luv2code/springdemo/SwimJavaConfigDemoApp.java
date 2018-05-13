327e388079910132d55c2097d68f2829e189f3a4
SunYuan <reggie.sunyuan@me.com>
2018-05-13 09:14:29 -0400

    IoC and DI using java configuration

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -0,0 +1,26 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SwimJavaConfigDemoApp {
+
+	public static void main(String[] args) {
+		// read spring config file
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+		
+		// get the bean from spring container
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
+		
+		// call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		
+		// check email and team
+		System.out.println("Email: " + theCoach.getEmail());
+		System.out.println("Team: " + theCoach.getTeam());
+		
+		// close the context
+		context.close();
+	}
+
+}
